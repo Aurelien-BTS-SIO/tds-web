965172906ee7182b7a789663336c1012e63c5b5d
Aurélien <113097057+Aurelien-BTS-SIO@users.noreply.github.com>
2022-12-13 16:52:04 +0100

    summit

--- td0/html site/contact.php
+++ td0/html site/contact.php
@@ -2,4 +2,30 @@
 
 <?php include "pages_utiles/menu.php"?>
 
+<div class="contactez-moi">
+<h1>Contactez-moi</h1>
+<p>une question, envie d'avoir d'avantage d'informations sur mon parcours. N’hésitez pas à utiliser ce formulaire pour prendre contact avec moi !</p>
+<form action="/page-traitement-donnees" method="post">
+<div>
+<label for="nom">Votre nom</label>
+<input type="text" id="nom" name="nom" placeholder="Nom" required>
+</div>
+<div>
+<label for="email">Votre e-mail</label>
+<input type="email" id="email" name="email" placeholder="email.com" required>
+</div>
+<div>
+<label for="sujet">Sujet</label>
+<input type="sujet" id="sujet" name="sujet" placeholder="" required>
+</div>
+<div>
+<label for="message">Votre message</label>
+<textarea id="message" name="message" placeholder="Bonjour, je vous contacte car...." required></textarea>
+</div>
+<div>
+<button type="submit">Envoyer mon message</button>
+</div>
+</form>
+</div>
+
 <?php include "pages_utiles/footer.php"?>
--- td0/html site/pages_utiles/header.php
+++ td0/html site/pages_utiles/header.php
@@ -12,6 +12,7 @@
         <link rel="stylesheet" href="Style\screen.css">
         <link rel="stylesheet" href="Style\Competences.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="Style\contact.css">
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@500&display=swap" rel="stylesheet">
     </head>
 <body>
