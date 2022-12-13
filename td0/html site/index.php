9d3f8fc3c0b2d4b157c161fb12d687322c25ec71
Aurélien <113097057+Aurelien-BTS-SIO@users.noreply.github.com>
2022-12-13 16:15:24 +0100

    Update index.php

--- td0/html site/index.php
+++ td0/html site/index.php
@@ -3,5 +3,13 @@
 
 <?php include "pages_utiles/menu.php"?>
 
+<h1>
+    Aurélien De Mil .
+</h1>
+<br>
+<h1>
+    Bienvenue sur le portfolio d'Aurélien De Mil étudiant en bts SIO 1.
+</h1>
 
 <?php include "pages_utiles/footer.php"?>
+
